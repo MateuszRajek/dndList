142e45d5df0765a90caef3a4824a6d1781a50fad
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-07-27 17:07:08 +0200

    Prepared main component for the view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import MainSection from "./components/MainSection/MainSection";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <div></div>;
+  const [activeScreen, setActiveScreen] = useState("boards");
+
+  const handleScreenChange = (screen) => {
+    setActiveScreen(screen);
+  };
+  return (
+    <div className="container">
+      <NavBar handleScreenChange={handleScreenChange} activeScreen={activeScreen} />
+      <MainSection activeScreen={activeScreen} />
+    </div>
+  );
 }
 
 export default App;
