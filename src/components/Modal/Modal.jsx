3fc58f891cf593cc139229d28471c09574829c73
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-07-27 19:06:28 +0200

    Handled closing modal without doing changes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,23 @@
 import Button from "../Button/Button";
+import CloseIcon from "../../assets/icons/Close.png";
 import "./Modal.css";
-function Modal({ onSubmit, inputValue, onInputChange }) {
+function Modal({ onSubmit, inputValue, onInputChange, closeModal }) {
   const handleInputChange = (event) => {
     onInputChange(event.target.value);
   };
   return (
     <div className="modal">
-      <form className="modal-form" onSubmit={onSubmit}>
-        <input onChange={handleInputChange} value={inputValue} className="modal-form-input" type="text" placeholder="Type something..." />
-        <Button>Save</Button>
-      </form>
+      <div className="modal-form-wrapper">
+        <form className="modal-form" onSubmit={onSubmit}>
+          <input onChange={handleInputChange} value={inputValue} className="modal-form-input" type="text" placeholder="Type something..." />
+          <Button>Save</Button>
+        </form>
+        <div className="close-icon-wrapper">
+          <Button handleClick={closeModal}>
+            <img className="modal-close-icon" src={CloseIcon} alt="close-icon" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
